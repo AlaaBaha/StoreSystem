bf5e0f85fc4fc8b52e83725610442cd2042de7c6
AlaaBaha <92575338+AlaaBaha@users.noreply.github.com>
2022-08-11 19:55:41 +0200

    resolve

--- src/secondpage/page1.java
+++ src/secondpage/page1.java
@@ -9,6 +9,7 @@ package secondpage;
 import add.Add1;
 import delete.delete1;
 import display.display1;
+import mainhome.Home;
 import modify.modify1;
 import report.showReport;
 
@@ -193,7 +194,8 @@ public class page1 extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        // TODO add your handling code here:
+new Home().setVisible(true);  
+  this.setVisible(false);// TODO add your handling code here:
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
